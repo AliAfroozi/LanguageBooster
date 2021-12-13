6c7194eba731c736f13796fe522e61c44a6b078e
Ali Afroozi <Ali.Afroozi.developer@gmail.com>
2021-12-13 12:04:01 +0330

    I had implement custom Adaptor and viewHolder

--- app/src/main/java/com/example/languagebooster/adaptors/RcyclerviewAdaptor.kt
+++ app/src/main/java/com/example/languagebooster/adaptors/RcyclerviewAdaptor.kt
@@ -1,8 +1,63 @@
 package com.example.languagebooster.adaptors
 
-class RcyclerviewAdaptor() {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.languagebooster.R
+import com.example.languagebooster.models.Word
+import soup.neumorphism.NeumorphImageButton
+import soup.neumorphism.NeumorphImageView
 
-    class WordHolder(){
+class RcyclerviewAdaptor(private val words: ArrayList<Word>) :
+    RecyclerView.Adapter<RcyclerviewAdaptor.WordHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordHolder {
+
+        return WordHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.recyclerview_item_row, parent, false)
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: WordHolder, position: Int) {
+        holder.keyword.text = "${words[position].keyword} : ${words[position].meaning}"
+        setStarImage(holder, words[position])
+        if (position == words.size - 1) {
+            holder.bordr.visibility = View.GONE
+        }
+
+
+        holder.star.setOnClickListener { star ->
+            words[position].stared = !words[position].stared
+            setStarImage(holder, words[position])
+        }
+    }
+
+
+    private fun setStarImage(holder: WordHolder, item: Word) {
+        holder.star.setImageResource(
+            if (item.stared) R.drawable.ic_baseline_star_24
+            else R.drawable.ic_baseline_star_border_24
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return words.size
+    }
+
+
+    class WordHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        var keyword: TextView = itemView.findViewById(R.id.item_text)
+        var star: NeumorphImageButton = itemView.findViewById(R.id.starBtn)
+        var img: NeumorphImageView = itemView.findViewById(R.id.img)
+        var bordr: View = itemView.findViewById(R.id.border)
+        override fun onClick(p0: View?) {
+            TODO("Not yet implemented")
+        }
 
     }
 }
--- app/src/main/java/com/example/languagebooster/data/MockData.kt
+++ app/src/main/java/com/example/languagebooster/data/MockData.kt
@@ -1,4 +1,15 @@
 package com.example.languagebooster.data
 
+import com.example.languagebooster.models.Word
+
 object MockData {
+    private val list = ArrayList<Word>()
+
+    fun addWord(word: Word) {
+        list.add(word)
+    }
+
+    fun getList(): ArrayList<Word> {
+        return list
+    }
 }
