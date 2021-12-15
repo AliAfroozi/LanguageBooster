d9a67ea7ab6d24d5a91b0bf32348490efa679a30
Ali Afroozi <Ali.Afroozi.developer@gmail.com>
2021-12-15 23:34:36 +0330

    A dialoge for exit and a splash screen have been added.

--- app/src/main/java/com/example/languagebooster/MainActivity.kt
+++ app/src/main/java/com/example/languagebooster/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.languagebooster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.languagebooster.adaptors.RcyclerviewAdaptor
@@ -24,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun init() {
         bindViews()
-        adaptor = RcyclerviewAdaptor(this , MockData.getList())
+        adaptor = RcyclerviewAdaptor(this, MockData.getList())
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = adaptor
@@ -43,11 +44,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun prepareItems() {
         MockData.getList().clear()
-        MockData.addWord(Word("Correct", "صحیح" , false))
-        MockData.addWord(Word("Mistake", "اشتباه" , false))
-        MockData.addWord(Word("Love", "زید" , false))
-        MockData.addWord(Word("Matrix", "جبر خطی" , false))
-        MockData.addWord(Word("Ali Afroozi", "توز " , false))
+        MockData.addWord(Word("Correct", "صحیح", false))
+        MockData.addWord(Word("Mistake", "اشتباه", false))
+        MockData.addWord(Word("Love", "زید", false))
+        MockData.addWord(Word("Matrix", "جبر خطی", false))
+        MockData.addWord(Word("Ali Afroozi", "توز ", false))
         adaptor.notifyDataSetChanged()
+    }
+
+
+    override fun onBackPressed() {
+        var alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle(R.string.close_app)
+        alertDialogBuilder.setMessage(R.string.close_app_Description)
+        alertDialogBuilder.setIcon(android.R.drawable.ic_dialog_alert)
+        alertDialogBuilder.setPositiveButton(R.string.yes) { _, _ ->
+            finish()
+        }
+        alertDialogBuilder.setNegativeButton(R.string.no) { _, _ -> }
+
+        val alertDialog : AlertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 }
