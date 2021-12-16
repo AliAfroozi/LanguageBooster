d45c5400eea3ae2eb8a7c196e5d2feff6519b0be
Ali Afroozi <Ali.Afroozi.developer@gmail.com>
2021-12-17 00:41:28 +0330

    adding a notif. is shown with app opening.

--- app/src/main/java/com/example/languagebooster/MainActivity.kt
+++ app/src/main/java/com/example/languagebooster/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.example.languagebooster
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.getSystemService
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.languagebooster.adaptors.RcyclerviewAdaptor
@@ -15,11 +24,37 @@ class MainActivity : AppCompatActivity() {
     private lateinit var fab: NeumorphFloatingActionButton
     private lateinit var adaptor: RcyclerviewAdaptor
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
 
+
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel =
+                NotificationChannel(
+                    1.toString(),
+                    "First usage",
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        val intent : Intent = Intent(this , MainActivity::class.java)
+        val pendingIntent : PendingIntent = PendingIntent.getActivity(this , 1 , intent , PendingIntent.FLAG_IMMUTABLE)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        val notification: Notification.Builder = Notification.Builder(this, 1.toString())
+            .setContentTitle(resources.getString(R.string.notification_title))
+            .setContentText(resources.getString(R.string.notification_description))
+            .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+            .setContentIntent(pendingIntent)
+
+        notificationManager.notify(1, notification.build())
 
     }
 
@@ -63,7 +98,9 @@ class MainActivity : AppCompatActivity() {
         }
         alertDialogBuilder.setNegativeButton(R.string.no) { _, _ -> }
 
-        val alertDialog : AlertDialog = alertDialogBuilder.create()
+        val alertDialog: AlertDialog = alertDialogBuilder.create()
         alertDialog.show()
     }
+
+
 }
